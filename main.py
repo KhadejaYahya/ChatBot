7535c882c09e5a8bbcd85d371161b9e18d5ca305
Khadeja Njaai <73859079+KhadejaYahya@users.noreply.github.com>
2022-08-18 21:55:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+from dotenv import load_dotenv
 import telebot
 import time
 import random
 
 load_dotenv()
-my_secret = os.getenv['API_KEY']
+my_secret = os.getenv('API_KEY')
 x = ["الله يعين", "الله يعين ، مدري شقولك بس الله يعين", "الله ييسرها"]
 
 random.seed(time.time())
